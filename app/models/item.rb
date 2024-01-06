961c93e014225856a508a2cdf1cd93a8c14d8c0e
haya73 <kmgo8810@gmail.com>
2024-01-06 20:08:40 +0900

    ログインしていない場合のリダイレクト

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,16 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_cost
+  belongs_to :prefecture
+  belongs_to :delivery_day
 
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :image,       presence: true
+  validates :item_name,   presence: true
+  validates :description, presence: true
+  validates :price,       presence: true
+
+  validates :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 
 end
