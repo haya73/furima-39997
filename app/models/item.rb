b670b50e220f6cc363439286548108ae6e3fee96
haya73 <kmgo8810@gmail.com>
2024-01-09 14:54:35 +0900

    エラーハンドリング

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,10 @@ class Item < ApplicationRecord
   validates :image,       presence: true
   validates :item_name,   presence: true
   validates :description, presence: true
-  validates :price,       presence: true
+  validates :price,       presence: true, numericality: {
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9999999,
+    only_integer: true }
 
   validates :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 
