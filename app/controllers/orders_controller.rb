2e07a3e3129be8f25ecde435270360f64d7988fe
haya73 <kmgo8810@gmail.com>
2024-01-30 22:08:08 +0900

    クレジットカード決済

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,20 @@
 class OrdersController < ApplicationController
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @orders_addresses = OrdersAddresses.new
     @item = Item.find(params[:item_id])
   end
 
   def create
     @orders_addresses = OrdersAddresses.new(order_params)
+    @item = Item.find(params[:item_id])
     if @orders_addresses.valid?
+      pay_item
       @orders_addresses.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -19,6 +23,16 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:orders_addresses).permit(:post_code, :prefecture_id, :city, 
-      :address, :telephone_number, :building, :order).merge(user_id: current_user.id, item_id: params[:item_id])
+      :address, :telephone_number, :building, :order)
+      .merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'            
+    )
   end
 end
--- app/models/orders_addresses.rb
+++ app/models/orders_addresses.rb
@@ -1,13 +1,13 @@
 class OrdersAddresses
   include ActiveModel::Model
   attr_accessor :post_code, :prefecture_id, :city, :address, :building,
-                :telephone_number, :user_id, :item_id, :order_id
+                :telephone_number, :user_id, :item_id, :order_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :telephone_number, format: {with: /\A\d{10,11}\z/, message: "は10桁以上11桁以内の数字で入力してください"}
-    validates :user_id, :item_id, :city, :address
+    validates :user_id, :item_id, :city, :address, :token
 
   end
 
