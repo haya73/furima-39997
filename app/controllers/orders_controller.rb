bbba28d382e1236f7adc2c1544c3706211b695c3
haya73 <kmgo8810@gmail.com>
2024-02-01 20:50:32 +0900

    トップページに遷移すること

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -41,7 +41,7 @@ class OrdersController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:item_id])
-    if @item.user_id == current_user.id
+    if @item.user_id == current_user.id || @item.order.present?
       redirect_to root_path
     end
   end
